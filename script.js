e03ee84261b3705b70301ca7666d59a7b903a644
Shaili-Patel <526shailipatel@frhsd.com>
2024-11-13 11:55:40 -0500

    Java Script Beginning Formation

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let userName = prompt('What is your name!?')
-let userQuest = prompt('What is your quest?')
+let userFriend = prompt('What is your friends name?')
 let userFavColor = prompt('What is your favorite color?')
 let swallowAirSpeed = prompt('What is the air speed of an unlaiden swallow?')
 
-let myStory = ` <p> Hello ${userName}. Answer me these questions three, and the other side you'll see.</p>
-<p>I see you have already answered. so your quest is to${userQuest}.</p>
+let myStory = ` <p> Hello ${userName}! You have been invited to the royal ball. along with your friend ${userFriend}</p>
+<p>I see you have already answered. so your quest is to ${userQuest}.</p>
 `
 
 console.log(myStory)
